fc73088491d22b64d0c086a457af23294091b278
Ahmad <Kingahmed19991@gmail.com>
2022-07-31 00:39:37 +0300

    going to work on logic part

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,10 @@
 const choices = [
-    './images\icon-rock.svg',
-    './images\icon-paper.svg',
-    './images\icon-scissors.svg'
+    './images/icon-rock.svg',
+    './images/icon-paper.svg',
+    './images/icon-scissors.svg'
 ]
 
+// THIS IS THE RULES WINDOW LOGIC
 document.querySelector('#rules-btn')
 .addEventListener('click', rulesWindow)
 document.querySelector('#close-btn')
@@ -19,6 +20,19 @@ function rulesWindow(e){
         rules.style.opacity = "1";
     }
 }
+// THIS IS THE RULES WINDOW LOGIC
 
 
+// THIS IS THE GAME LOGIC
+document.querySelectorAll('rps')
+.forEach(el =>{
+    el.addEventListener('click', rps_handler)
+})
+function rps_handler(e){
+document.querySelector('.rps-choice').style.display = 'none'
+
+}
+
+
+// THIS IS THE GAME LOGIC
 
