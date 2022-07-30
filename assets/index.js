bbc10f6e027c4ea7eee9865bd7680c725730d429
Ahmad <Kingahmed19991@gmail.com>
2022-07-30 14:04:54 +0300

    --amend

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,24 @@
 const choices = [
-    './images/icon-rock.svg',
-    './images/icon-paper.svg',
-    './images/icon-scissors.svg'
+    './images\icon-rock.svg',
+    './images\icon-paper.svg',
+    './images\icon-scissors.svg'
 ]
+
+document.querySelector('#rules-btn')
+.addEventListener('click', rulesWindow)
+document.querySelector('#close-btn')
+.addEventListener('click',rulesWindow)
+function rulesWindow(e){
+    var rules = document.querySelector('#rules-container')
+    if(rules.style.display === "flex"){
+        rules.style.opacity = "0";
+        rules.style.display = "none";
+        
+    }else{
+        rules.style.display = "flex";
+        rules.style.opacity = "1";
+    }
+}
+
 
 
