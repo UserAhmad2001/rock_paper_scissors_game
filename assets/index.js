783d361fc69ccd2d5eea6b04c94454427a3e59db
Ahmad <Kingahmed19991@gmail.com>
2022-07-31 14:34:35 +0300

    javascript setup almost complete

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,15 @@ const choices = [
     './images/icon-paper.svg',
     './images/icon-scissors.svg'
 ]
+var rps_choice = document.querySelector('.rps-choice')
+var rps_results = document.querySelector('.rps-results')
+var house_picked = document.querySelector('.house-choice-img')
+var player_picked = document.querySelector('.player-choice-img')
+var result_sec = document.querySelector('.result-txt-btn') 
+var res_txt = document.querySelector('.result')
+var score = document.querySelector('.score')
+var mChoice = document.querySelector('.my-choice')
+var hChoice = document.querySelector('.house-choice')
 
 // THIS IS THE RULES WINDOW LOGIC
 document.querySelector('#rules-btn')
@@ -24,15 +33,102 @@ function rulesWindow(e){
 
 
 // THIS IS THE GAME LOGIC
-document.querySelectorAll('rps')
+document.querySelector('.play-again')
+.addEventListener('click',play_again)
+document.querySelectorAll('.rps')
 .forEach(el =>{
     el.addEventListener('click', rps_handler)
 })
 function rps_handler(e){
-document.querySelector('.rps-choice').style.display = 'none'
+player_choice = e.currentTarget.children[0].src
+player_picked.children[0].src = player_choice
+rps_choice.style.display = 'none'
+rps_results.style.display = 'flex'
 
+setTimeout(() => {
+    var house_picked = document.querySelector('.house-choice-img')
+    house_picked.style.display = 'flex'
+    var rand = choices[Math.floor(Math.random() * 2)]
+    house_picked.children[0].src = rand
+    result_sec.style.display = 'flex'
+    changeScore(rand, player_choice)
+}, 3000);
 }
 
+function play_again(e){
+    result_sec.style.display = 'none'
+    rps_results.style.display = 'none'
+    rps_choice.style.display = 'flex'
+    house_picked.style.display = 'none'
+    document.querySelector('.focused').remove()
+}
+
+function changeScore(rand , player_ch){
+
+    if(player_ch.includes('rock') && rand === choices[0]){
+        res_txt.innerHTML = "IT'S A DRAW"
+    }
+    else if(player_ch.includes('paper') && rand === choices[1]){
+        res_txt.innerHTML = "IT'S A DRAW"
+    }
+    else if(player_ch.includes('scissors') && rand === choices[2]){
+        res_txt.innerHTML = "IT'S A DRAW"
+    }
+    else if(player_ch.includes('rock') && rand === choices[1]){
+        res_txt.innerHTML = "YOU LOST"
+        var current_score = Number.parseInt(score.innerHTML)
+        current_score--
+        score.innerHTML = current_score
+        focusOnWinner(false)
+    }
+    else if(player_ch.includes('rock') && rand === choices[2]){
+        res_txt.innerHTML = "YOU WON"
+        var current_score = Number.parseInt(score.innerHTML)
+        current_score++
+        score.innerHTML = current_score
+        focusOnWinner(true)
+    }
+    else if(player_ch.includes('paper') && rand === choices[0]){
+        res_txt.innerHTML = "YOU WON"
+        var current_score = Number.parseInt(score.innerHTML)
+        current_score++
+        score.innerHTML = current_score
+        focusOnWinner(true)
+    }
+    else if(player_ch.includes('paper') && rand === choices[2]){
+        res_txt.innerHTML = "YOU LOST"
+        var current_score = Number.parseInt(score.innerHTML)
+        current_score--
+        score.innerHTML = current_score
+        focusOnWinner(false)
+    }
+    else if(player_ch.includes('scissors') && rand === choices[0]){
+        res_txt.innerHTML = "YOU LOST"
+        var current_score = Number.parseInt(score.innerHTML)
+        current_score--
+        score.innerHTML = current_score
+        focusOnWinner(false)
+    }
+    else if(player_ch.includes('scissors') && rand === choices[1]){
+        res_txt.innerHTML = "YOU WON"
+        var current_score = Number.parseInt(score.innerHTML)
+        current_score++
+        score.innerHTML = current_score
+        focusOnWinner(true)
+    }
+}
+function focusOnWinner(player){
+    var el = document.createElement('span')
+    el.classList = 'focused'
+
+    if(player){
+        mChoice.appendChild(el)
+    }
+    else{
+        hChoice.appendChild(el)
+    }
+
+}
 
 // THIS IS THE GAME LOGIC
 
